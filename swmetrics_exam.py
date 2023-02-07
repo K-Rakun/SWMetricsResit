9fb6df00435b05b8fefe63e888d61e16b0eac8dc
Erman Şen <61985023+K-Rakun@users.noreply.github.com>
2023-02-07 11:22:26 +0300

    Add files via upload

--- swmetrics_exam.py
+++ swmetrics_exam.py
@@ -7,6 +7,10 @@ class Tests(unittest.TestCase):
     def test_wrapper_correct_args(self):
         self.assertEqual(wrapper(3), 6)
 
+    def test_wrapper_incorrect_args(self):
+        with self.assertRaises(TypeError):
+            wrapper("three", 6)
+
 
 if __name__ == '__main__':
     unittest.main()
