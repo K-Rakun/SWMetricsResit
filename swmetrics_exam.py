4c07ab67d9994c21207c0aa7038c50e958971c26
Erman Şen <61985023+K-Rakun@users.noreply.github.com>
2023-02-07 11:51:27 +0300

    Add files via upload

--- swmetrics_exam.py
+++ swmetrics_exam.py
@@ -1,24 +1,25 @@
+"""Exam"""
+
 import unittest
 from math_test import wrapper
 
-
 class Tests(unittest.TestCase):
-
+    """Test cases"""
     def test_wrapper_correct_args(self):
+        """Correct Inputs"""
         self.assertEqual(wrapper(3), 6)
-
     def test_wrapper_incorrect_args(self):
+        """Incorrect Inputs"""
         with self.assertRaises(TypeError):
-            wrapper("three", 6)
-
+            wrapper("three")
     def test_wrapper_negative_args(self):
+        """Factorial input can't be negative"""
         with self.assertRaises(TypeError):
-            wrapper("-3", 6)
-
+            wrapper("-3")
     def test_wrapper_without_args(self):
+        """Input needed"""
         with self.assertRaises(TypeError):
-            wrapper()
-
+            wrapper("")
 
 if __name__ == '__main__':
     unittest.main()
